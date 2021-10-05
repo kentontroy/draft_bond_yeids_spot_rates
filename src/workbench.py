7c7295423f36713f9a845c23760484507748902c
Kenton Troy <kentontroy@yahoo.com>
2021-10-04 20:35:18 -0400

    Update workbench.py

--- src/workbench.py
+++ src/workbench.py
@@ -1,23 +1,25 @@
-from	bond	import	Bond
-from	utility	import	lambda_reverse_range
+from bond import Bond
+from utility import lambda_reverse_range
 
-class	WorkBench:
-		def	findUpperBoundForYield(bond,	boundForPrice):
-				for	v	in	lambda_reverse_range():
-						presentValue	=	bond.getPresentValue(v)
-						if	(presentValue	>	boundForPrice):
-								print	"Bond	Price:	%f"	%	presentValue
-								print	"Yield:	%f"	%	v
-								break
-		findUpperBoundForYield	=	staticmethod(findUpperBoundForYield)
-    
-		def	getTermStructureFromSpotRates(spotRateCurve):
-				print	spotRateCurve.getRates()
-				estimate	=	spotRateCurve
-				try:
-						while(1):
-								estimate	=	estimate.estimateNextYear()
-								print	estimate.getRates()
-				except:
-						pass
-		getTermStructureFromSpotRates	=	staticmethod(getTermStructureFromSpotRate
+class WorkBench:
+  def findUpperBoundForYield(bond, boundForPrice):
+    for	v in lambda_reverse_range():
+      presentValue = bond.getPresentValue(v)
+      if (presentValue	> boundForPrice):
+	print "Bond Price: %f"	% presentValue
+	print "Yield: %f" % v
+	break
+     
+    findUpperBoundForYield = staticmethod(findUpperBoundForYield)
+
+    def	getTermStructureFromSpotRates(spotRateCurve):
+      print spotRateCurve.getRates()
+      estimate	= spotRateCurve
+      try:
+	while(1):
+	  estimate = estimate.estimateNextYear()
+	  print	estimate.getRates()
+       except:
+	  pass
+	
+     getTermStructureFromSpotRates = staticmethod(getTermStructureFromSpotRate
